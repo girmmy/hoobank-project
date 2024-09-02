c6a4e4c7c80bd4d07f91b0ce080fb2baace6dd21
gimmy <gimmys943@gmail.com>
2024-09-02 16:34:50 -0400

    initial commit and completed project

---
+++ src/components/Business.jsx
@@ -0,0 +1,28 @@
+import styles, { layout } from '../style';
+import { features } from  '../constants';
+import Button from './Button';
+import FeatureCard from './FeatureCard';
+
+const Business = () => (
+    <section id="features" className={layout.section} >
+      <div className={layout.sectionInfo}>
+        <h2 className={`${styles.heading2}`}>
+          You do the business, <br className="sm:block hidden"/> 
+        we’ll handle the money.</h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+        With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.
+        </p>
+
+        <Button styles="mt-10"/>
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+)
+
+
+export default Business
---
+++ src/components/FeatureCard.jsx
@@ -0,0 +1,28 @@
+import styles from '../style';
+import { features } from  '../constants';
+import PropTypes from 'prop-types';
+
+const FeatureCard = ({ icon, title, content, index }) => (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex flex-col ml-3">
+        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+          {title}
+        </h4>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+  
+FeatureCard.propTypes = {
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+};
+
+export default FeatureCard;
